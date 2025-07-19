ca85bc4276824388f4a16f78b4d9e17f983337e3
Aditi Paraskar <aditiparaskar88@gmail.com>
2025-07-19 19:04:25 +0530

    Add files via upload

---
+++ caluction.py
@@ -0,0 +1,21 @@
+number1=11
+number2=22
+sum_result= number1+number2
+print(sum_result)
+
+number1=44
+number2=66
+sub_result=number1-number2
+print(sub_result)
+
+number1=78
+number2=99
+multiple_result=number1*number2
+print(multiple_result)
+
+number1=788
+number2=888
+divison_result=number1/number2
+print(divison_result)
+
+
